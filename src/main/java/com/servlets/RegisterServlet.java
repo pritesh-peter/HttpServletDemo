d18b0705a8b2c9d789b41e9ab89c4d21f6ad2c06
pritesh-peter <pritesh.peter@gmail.com>
2022-08-09 22:08:54 +0545

    requestdispatcher include and forward

--- src/main/java/com/servlets/RegisterServlet.java
+++ src/main/java/com/servlets/RegisterServlet.java
@@ -19,11 +19,35 @@ public class RegisterServlet extends HttpServlet {
      String condition = request.getParameter("user_condition");
      
      System.out.println(condition);
+     if(condition!=null){
      if("checked".equals(condition) ){
+     out.println("<h2>Name : "+name+"</h2>");
+     out.println("<h2>Email : "+email+"</h2>");
+     out.println("<h2>Gender : "+gender+"</h2>");
+     out.println("<h2>Course : "+course+"</h2>");
      
+     //JDBC///
+     
+     //after saved to db
+     
+     RequestDispatcher rd = request.getRequestDispatcher("success");
+     rd.forward(request, response);
      }
      else{
               out.println("<h2>Agreement must be checked</h2>");
+     }
+     }
+     else{
+                   out.println("<h2>Agreement must be checked</h2>");
+                   //include output of index.html
+                   
+                   //Get the object of RequestDispatcher
+                   
+                   RequestDispatcher rd = request.getRequestDispatcher("index.html");
+                   
+                   //include method
+                   
+                   rd.include(request, response);
      }
     }
 }
